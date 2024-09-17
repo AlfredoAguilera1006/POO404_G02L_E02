2b33f082bef5a259781d846f74fd6886f903b388
Alfredo Aguilera <alfredo100206@gmail.com>
2024-09-17 17:07:02 -0600

    Inicio de codigo

---
+++ Login/src/Login.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Login {
+    private JPanel Login;
+    private JTextField txtUsuario;
+    private JTextField txtContraseña;
+    private JButton btnIngresar;
+}
